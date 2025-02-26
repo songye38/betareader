1638bc82abe6cae596c123c95b03fb0519e80de6
songye38 <ddev0308@gmail.com>
2025-02-26 18:51:53 +0900

    - 서체 변경 - margin 변경 - main, manu 페이지 세팅

---
+++ src/app/main/page.js
@@ -0,0 +1,12 @@
+// pages/main.js
+const MainPage = () => {
+    return (
+      <div>
+        <h1>Main Page</h1>
+        <p>여기는 MainPage입니다. /main 경로로 접근됩니다.</p>
+      </div>
+    );
+  };
+  
+  export default MainPage;
+  
---
+++ src/app/manu/page.js
@@ -0,0 +1,12 @@
+// pages/manu.js
+const ManuscriptPage = () => {
+    return (
+      <div>
+        <h1>Manuscript Page</h1>
+        <p>여기는 ManuscriptPage입니다. /manu 경로로 접근됩니다.</p>
+      </div>
+    );
+  };
+  
+  export default ManuscriptPage;
+  
