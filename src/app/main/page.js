5d5a99ed647dd3babd8096c739ee010284c67cd4
songye38 <ddev0308@gmail.com>
2025-02-26 19:30:47 +0900

    원고집이 들어가는 EpisodeTab 완료

--- src/app/main/page.js
+++ src/app/main/page.js
@@ -1,11 +1,14 @@
 
 import Button1 from "@/components/Buttons/Button1";
+import EpisodeTab from "@/components/EpisodeTab";
 
 // pages/main.js
 const MainPage = () => {
     return (
       <div>
         <Button1 />
+        <EpisodeTab label="설정집" selected={true} />
+        <EpisodeTab label="원고집" selected={false} />
       </div>
     );
   };
---
+++ src/components/EpisodeTab.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const EpisodeTab = ({ label = '설정집', selected = false }) => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        width: '13.75rem',
+        borderRadius: '0.75rem',
+        height: '2.25rem',
+        padding: '0.5rem 0.8125rem',
+        alignItems: 'center',
+        gap: '0.625rem',
+        background: selected
+          ? 'var(--neutral-700, #3A3D46)' // 선택된 탭의 배경색
+          : 'var(--neutral-800, #2C2D34)', // 기본 배경색
+        cursor: 'pointer',
+      }}
+    >
+      {/* 텍스트 */}
+      <div
+        style={{
+          color: 'white',
+          fontSize: '14px',
+          fontFamily: 'Pretendard',
+          fontWeight: '500',
+          lineHeight: '19.6px',
+          wordWrap: 'break-word',
+        }}
+      >
+        {label}
+      </div>
+    </div>
+  );
+};
+
+export default EpisodeTab;
