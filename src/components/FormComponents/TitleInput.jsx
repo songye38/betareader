08919ae606196480f4e8ef48a03c2a0500c5a91f
songye38 <ddev0308@gmail.com>
2025-02-27 20:15:25 +0900

    showLabel에 따라 제목의 길이가 바뀌도록 함 - true이면 959 - false면 850으로

--- src/components/FormComponents/TitleInput.jsx
+++ src/components/FormComponents/TitleInput.jsx
@@ -6,7 +6,9 @@ import { Controller } from 'react-hook-form';
 
 const TitleInput = ({ control, error,showLabel = true }) => {
   return (
-    <div style={{ width: '850px', height: '76px', paddingLeft: 28, paddingRight: 28, paddingTop: 24, paddingBottom: 24, background: '#1E1F24', borderRadius: 20, overflow: 'hidden', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 16, display: 'inline-flex' }}>
+    <div style={{ 
+        width: showLabel ? '959px' : '850px',
+        height: '76px', paddingLeft: 28, paddingRight: 28, paddingTop: 24, paddingBottom: 24, background: '#1E1F24', borderRadius: 20, overflow: 'hidden', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 16, display: 'inline-flex' }}>
       {showLabel && (
         <div style={{ color: 'white', fontSize: 24, fontFamily: 'Pretendard', fontWeight: '700', lineHeight: '33.60px', wordWrap: 'break-word' }}>
           제목
